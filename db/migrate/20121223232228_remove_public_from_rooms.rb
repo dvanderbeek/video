ae83fdd629f35e792acdaff66d74b75e957cfefd
David Van Der Beek <earlynovrock@gmail.com>
2012-12-23 17:23:28 -0600

    remove public from rooms table

---
+++ db/migrate/20121223232228_remove_public_from_rooms.rb
@@ -0,0 +1,9 @@
+class RemovePublicFromRooms < ActiveRecord::Migration
+  def up
+    remove_column :rooms, :public
+  end
+
+  def down
+    add_column :rooms, :public, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121223174341) do
+ActiveRecord::Schema.define(:version => 20121223232228) do
 
   create_table "rooms", :force => true do |t|
     t.string   "name"
     t.string   "session_id"
-    t.boolean  "public"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
